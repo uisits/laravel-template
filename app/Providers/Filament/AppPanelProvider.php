9b62fc027af03e7597941ae847cd8da8fd6adc2f
ChinwalPrasad <prasadchinwal5@gmail.com>
2024-02-27 10:08:10 -0600

    Added UIS footer

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Filament\Facades\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -11,6 +12,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Illuminate\Contracts\View\View;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -40,6 +42,10 @@ class AppPanelProvider extends PanelProvider
                 'primary' => Color::Indigo,
                 'gray' => Color::Slate,
             ])
+            ->renderHook(
+                'panels::body.end',
+                fn () => view('filament.components.footer'),
+            )
             ->favicon(asset('/favicon.ico'))
             ->brandLogoHeight('40px')
             ->brandLogo(fn () => view('filament.logo.light'))
---
+++ resources/views/filament/components/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+    <div class="mx-auto max-w-7xl px-6 py-10 md:flex items-center justify-between lg:px-8 space-y-3">
+
+        <div class="md:w-1/3 text-center">
+            <div>
+                <span class="text-muted font-bold text-lg">University of Illinois Springfield</span>
+                <br/>
+                <span class="font-semibold text-md">One University Plaza</span><br/>
+                <span class="font-semibold text-xs">Springfield, Illinois 62703</span><br/>
+                <a href="tel:217-206-6600">P: 217-206-6600</a>
+            </div>
+        </div>
+
+        <div class="md:w-1/3 text-center">
+            <div>
+                <span class="text-muted text-lg font-bold">Developed by </span>
+                <br/>
+                <a href="http://www.uis.edu/its/">Information Technology Services</a>
+                <br>
+                <span class="font-semibold text-xs">© University of Illinois Springfield</span><br />
+                <span style="font-size: x-small">Version TL-2-21-24-{{ env('CONTAINER_NAME') }}</span>
+                <a class="optanon-show-settings">Cookie Settings</a>
+            </div>
+        </div>
+    </div>
+</footer>
